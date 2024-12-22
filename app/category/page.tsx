3c23f1613a874421a81420c2190fb2f5ee585ad9
sutaaa0 <sutadev0@gmail.com>
2024-12-22 11:34:26 +0700

    feat: pass user ID to CategoryPage for personalized content

--- app/category/page.tsx
+++ app/category/page.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import CategoryPage from "../pages/category";
+import { auth } from "@/auth";
 
-const page = () => {
-  return <CategoryPage />;
+const page = async () => {
+
+  const session = await auth();
+  const userId = session?.user?.id;
+
+  return <CategoryPage userId={userId as string} />;
 };
 
 export default page;
